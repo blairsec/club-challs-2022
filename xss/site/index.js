77da03b7f0c16c40aba6bf3b7ab61b42229f58b2
Patrick Zhang <zhangpatrick2004@gmail.com>
2022-10-05 23:46:15 -0400

    xss initial commit

---
+++ xss/site/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import exphbs from "express-handlebars";
+import path from 'path';
+
+const app = express()
+app.engine('hbs', exphbs.create({ extname: "hbs" }).engine) ;
+app.set("view engine", "hbs");
+const port = 3000;
+app.use(express.static("./static"));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/one', (req, res) => {
+  // no restrictions
+  res.render('one', { edit: !req.query.c, content: req.query.c });
+});
+
+app.get('/two', (req, res) => {
+  // add blacklist
+  
+  res.render('two', { edit: !req.query.c, content: req.query.c });
+});
+
+app.get('/three', (req, res) => {
+  // more blacklist
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/four', (req, res) => {
+  // something else
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`)
+});
